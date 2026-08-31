e3f2e10ed6b84b1a3586a263ae15f493556e207c
Krishna Gupta <krishnag1902@gmail.com>
2026-09-01 05:13:39 +0530

    Clean up ListNode definition and whitespace
    Removed commented definition of ListNode and cleaned up whitespace.

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -1,21 +1,9 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode dummy = new ListNode(0);
         dummy.next =head;
-
-
-     
-
+        
         ListNode l = head;
         int jumps = 0;
 // ll lenth calculate
@@ -23,7 +11,6 @@ class Solution {
             l=l.next;
             jumps++;
         }
-       
 
         ListNode prev = dummy,
                  curr = l;
