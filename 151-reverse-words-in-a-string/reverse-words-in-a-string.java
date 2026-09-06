4f78a21aa2c0cf331bc3de0dd3e80347dec165d2
Krishna Gupta <krishnag1902@gmail.com>
2026-09-06 18:36:33 +0530

    Time: 9 ms (22.31%) | Memory: 43.7 MB (96.85%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,101 @@
+class Solution {
+    
+    public String reverseWords(String s) {
+        //trim
+        //l, r, s
+        //sb -> s cleaned with multiple spaces to a single space
+        //reverse sb
+        //reverse word by word in sb
+
+        int l=0,
+            r = s.length()-1;
+        // "  the hello  "
+        //    l
+        //            r
+        while(l<s.length()){
+            if(s.charAt(l)==' '){
+                l++;
+            }else{
+                break;
+            }
+
+        }
+        while(r>=0){
+            if(s.charAt(r)==' '){
+                r--;
+
+            }else{
+                break;
+            }
+        }
+
+        StringBuilder sB = new StringBuilder();
+        // "  the   hello  "
+        //                l
+        //                r
+        // sb-> "the hello "
+        while(l<=r){
+            if(s.charAt(l)!=' '){
+                sB.append(s.charAt(l));
+                l++;
+            }else if(s.charAt(l)==' '){
+                if(sB.charAt(sB.length()-1)!=' '){
+                    sB.append(' ');
+                    l++;
+                
+                }else{
+                l++;
+                }
+            }  
+        }
+
+        //sb-> "the hello"
+        //      i       j 
+        //reverse
+        int i=0,
+            j=sB.length()-1;
+        while(i<j){
+            char temp = sB.charAt(i);
+            sB.setCharAt(i, sB.charAt(j));
+            sB.setCharAt(j, temp);
+            i++;
+            j--;
+
+        }
+        //sb-> "olleh eth"
+        // word by word rev 
+
+        //sb-> "olleh eth"
+        
+
+
+
+
+
+
+        //sb-> "olleh eth"
+        //      s
+        //      e
+        int start = 0,
+            end = 0;
+        while(start<sB.length()){
+            while(end<sB.length() &&  sB.charAt(end)!= ' ' ){
+                    end++;
+            }
+            int p1 = start,
+                p2 = end-1;
+            while(p1<p2){
+                char temp = sB.charAt(p1);
+                sB.setCharAt(p1, sB.charAt(p2));
+                sB.setCharAt(p2, temp);
+                p1++;
+                p2--; 
+            }
+            start = end+1;
+            end = start;
+        }
+            
+        
+        return sB.toString();
+    }
+}
