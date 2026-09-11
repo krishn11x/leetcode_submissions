ab038cfb70d75ac44861c36aa5676a7d61107962
Krishna Gupta <krishnag1902@gmail.com>
2026-09-11 22:17:18 +0530

    Create cycle_length.java
    this is a different question from this 142, it is called  Cycle Length in Linked List from gfg.

---
+++ 142-linked-list-cycle-ii/cycle_length.java
@@ -0,0 +1,41 @@
+/* Structure of Linked List Node
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}*/
+
+class Solution {
+    public int lengthOfLoop(Node head) {
+        // code here
+        Node slow = head,
+                fast = head;
+        while(fast!= null && fast.next.next!=null){
+            slow = slow.next;
+            fast=fast.next.next;
+            if(slow == fast){
+                break;
+            }
+        }
+        if (fast == null || fast.next == null){
+            return 0;
+        }
+        Node n1 = slow,
+                n2 = head;
+        while(n1!=n2){
+            n1=n1.next;
+            n2=n2.next;
+        }
+        int len=1;
+        Node curr = n1.next;
+        while(n1!=curr){
+            len++;
+            curr = curr.next;
+        }
+        return len;
+    }
+}
