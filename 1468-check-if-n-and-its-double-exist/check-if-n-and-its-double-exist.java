f38fc3450a3f779a623d7ce0ca7bdd1760871889
Krishna Gupta <krishnag1902@gmail.com>
2026-09-19 02:47:45 +0530

    Add newline at end of check-if-n-and-its-double-exist.java

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -15,3 +15,18 @@ class Solution {
         return false;
     }
 }
+
+// brute force code
+/*class Solution {
+    public boolean checkIfExist(int[] arr) {
+     for(int i = 0; i<arr.length; i++){
+        int dv = arr[i]*2;
+        for(int j = 0; j<arr.length; j++){
+            if(arr[j]==dv && j!=i){
+                return true;
+            }
+        }
+     }   
+     return false;
+    }
+}*/
