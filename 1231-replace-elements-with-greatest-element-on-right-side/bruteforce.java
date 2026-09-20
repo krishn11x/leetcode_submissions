31ffef6efe22f1d7ebe32d2ab9973c240be817d0
Krishna Gupta <krishnag1902@gmail.com>
2026-09-21 01:54:36 +0530

    Update bruteforce.java

--- 1231-replace-elements-with-greatest-element-on-right-side/bruteforce.java
+++ 1231-replace-elements-with-greatest-element-on-right-side/bruteforce.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
         //0, 1, 2,3,4,5
-        //18
+        //18  6 6 6 1 -1
         //17,18,5,4,6,1
-        //   i
-        //        j
+        //          i
+        //              j
 
-        for(int i = 0; i<arr.length; i++){//i = 1
-                int temp = -1; 
-            for(int j = i+1; j<arr.length; j++){// j = 3
+        for(int i = 0; i<arr.length; i++){//i = 5
+                int temp = -1; // temp = 18/ -1/ 5/ 6 / -1/ 4/ 6/ -1/ 6/ -1/ 1/ -1
+            for(int j = i+1; j<arr.length; j++){// j = 6
                 
                 if(arr[j]>temp){
                     temp = arr[j];
